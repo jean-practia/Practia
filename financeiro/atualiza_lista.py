5920abc86354c21f02ebb91d2e2556b2bfbd3839
Jean_Carlos <79288480+jean-script@users.noreply.github.com>
2022-10-27 19:49:34 -0300

    Atualizando path

--- financeiro/atualiza_lista.py
+++ financeiro/atualiza_lista.py
@@ -1,12 +1,15 @@
 import win32com.client
 import time
 
+path_practia ='C:\\Users\\USER\\Documents\\Estudos\\python\\financeiro\\input\\controleFinanceiropy.xlsx'
+path_jean ='C:\\Users\\jean\\arquivos\\Documentos\\practia\\Practia\\financeiro\\input\\controleFinanceiropy.xlsx'
+
 def atualiza_dados():
     """Obtendo os dados do sharepoint para uma planilha"""
     sucesso = False
     try:
         xlapp = win32com.client.DispatchEx("Excel.Application")
-        wb = xlapp.Workbooks.Open('C:\\Users\\USER\\Documents\\Estudos\\python\\financeiro\\input\\controleFinanceiropy.xlsx')
+        wb = xlapp.Workbooks.Open(path_jean)
         wb.RefreshAll()
 
         xlapp.CalculateUntilAsyncQueriesDone()
